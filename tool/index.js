0091847e2a1a7d5849f64017356d611663872d07
yemiancheng <ymc.github@gmail.com>
2019-07-04 11:18:37 +0800

    build(editorconfig): witring editorconfig settings
       sets end_of_line to lf
       sets comment in .editorconfig

--- tool/index.js
+++ tool/index.js
@@ -1,19 +1,19 @@
 /* eslint-disable no-console */
 /* eslint-disable import/no-extraneous-dependencies */
-const read = require('@commitlint/read')
-const lint = require('@commitlint/lint')
-const { rules } = require('../commitlint.config.js')
-const { addFile, commit } = require('./git')
+const read = require('@commitlint/read');
+const lint = require('@commitlint/lint');
+const { rules } = require('../commitlint.config.js');
+const { addFile, commit } = require('./git');
 
 read({ edit: '.git/COMMIT_EDITMSG' })
-    .then(content => lint(content[0], rules))
-    .then(msg => addFile(['tool/index.js','tool/addOrigin.js','tool/setOrigin.js','tool/shields.js', 'index.js','commitlint.config.js','package.json','readme.md','.travis.yml']).then(() => msg))
-    .then((result) => {
-        if (result.valid) {
-            return commit(result.input)
-        }
-        console.log(result)
-        return result
-    })
-    .then(report => console.log(report.stdout))
-    .catch(console.log)
+  .then(content => lint(content[0], rules))
+  .then(msg => addFile(['tool/index.js', '.editorconfig']).then(() => msg))
+  .then((result) => {
+    if (result.valid) {
+      return commit(result.input);
+    }
+    console.log(result);
+    return result;
+  })
+  .then(report => console.log(report.stdout))
+  .catch(console.log);
