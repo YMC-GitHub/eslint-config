e66c06878147ffc0880aec63d986912594b4375e
yemiancheng <ymc.github@gmail.com>
2019-07-12 09:29:14 +0800

    build(git,console): updating git tool
      renames ./tool/firstPush.js to ./tool/push.js
      creates ./tool/console.js for log
      updates ./tool/listOrigin.js with console.js log
      updates ./tool/setOrigin.js with console.js log
      updates ./tool/showOrigin.js with console.js log

--- tool/firstPush.js
+++
@@ -1,4 +0,0 @@
-/* eslint-env node */
-const { push } = require('./git');
-
-push(['--set-upstream', 'origin', 'master']);
--- tool/listOrigin.js
+++ tool/listOrigin.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-console */
+
 /* eslint-env node */
 const { remote } = require('./git');
-
-remote([])
-  .then(report => console.log(report))
-  .catch(err => console.log(err));
+const { logAsync } = require('./console');
+logAsync(remote([]));
